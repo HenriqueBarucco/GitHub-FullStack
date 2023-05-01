4925459f2137d12952a8299725c65bb1e40fe7cc
Henrique Barucco <henrique_barucco@hotmail.com>
2023-05-01 19:56:16 -0300

    🔧 chore(api): usa variável de ambiente para a URL da API Agora a URL da API é definida por meio da variável de ambiente process.env.API, o que permite que a aplicação seja mais flexível e possa ser executada em diferentes ambientes sem a necessidade de alterar o código. As rotas /api/user/details/[username], /api/user/repos/[username] e /api/users agora usam a variável de ambiente para definir a URL da API.

--- frontend-next/pages/api/user/repos/[username].js
+++ frontend-next/pages/api/user/repos/[username].js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function handler(req, res) {
   const { username } = req.query;
   const response = await axios.get(
-    `http://backend:8080/api/users/${username}/repos`
+    `${process.env.API}/api/users/${username}/repos`
   );
   const data = response.data;
   res.status(200).json(data);
--- frontend-next/pages/api/users.js
+++ frontend-next/pages/api/users.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function handler(req, res) {
   const { since } = req.query;
   const response = await axios.get(
-    `http://backend:8080/api/users?since=${since}`
+    `${process.env.API}/api/users?since=${since}`
   );
   const data = response.data;
   res.status(200).json(data);
